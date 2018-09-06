e543e75f70ced6968502eed1de83d5c707f17182
Theophelus <aneletom10@gmail.com>
2018-09-06 11:45:05 +0200

    importing modules

---
+++ index.js
@@ -0,0 +1,6 @@
+//import modules
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');

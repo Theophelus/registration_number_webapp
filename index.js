2e4112630abde6f8824e9116970603615c79574b
Theophelus <aneletom10@gmail.com>
2018-09-06 11:48:23 +0200

    configuring imported modules and middlewares

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const session = require('express-session');
+
+//define instances
+const pg = require("pg");
+const Pool = pg.Pool;
+let app = express();
+
+// initialise session middleware - flash-express depends on it
+app.use(session({
+    secret: "<add a secret string here>",
+    resave: false,
+    saveUninitialized: true
+}));
+// initialise the flash middleware
+app.use(flash());
+

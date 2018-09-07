779bfbc90637df5f7973f82461d545b6db3b7011
Theophelus <aneletom10@gmail.com>
2018-09-07 12:12:15 +0200

    updating todo list file and add new js file for logic

---
+++ public/reg_number.js
@@ -0,0 +1,12 @@
+module.exports = function (pool) {
+    //define a function call setReg with one parameter
+    let setReg = async (regNumbers) => {
+        //create regEx for numbers
+        RegNumbers = /^[a-zA-Z]{2,3}(\s)[0-9]{3}(\-)[0-9]{3}$/;
+
+    }
+
+    return {
+        
+    }
+}
---
+++ todo_list.js
@@ -0,0 +1,4 @@
+// 1) define a function call setRegistrations that that take in one parameter call numbers
+        // - check if registration number fall within our scope.
+        // - then if that condition is true, if true add that registration
+        // - else false display a message registreation already exists

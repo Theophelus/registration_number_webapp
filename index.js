f932b26e10d53e0e29c58b8acfdede72b11446d3
Theophelus <aneletom10@gmail.com>
2018-09-07 16:07:19 +0200

    updating factory function and adding a post route in my index.js file to enter registrations into th server to database

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const session = require('express-session');
-
+const Reg_numbers = require('./public/reg_number');
 //define instances
 const pg = require("pg");
 const Pool = pg.Pool;
@@ -18,6 +18,20 @@ app.use(session({
 }));
 // initialise the flash middleware
 app.use(flash());
+et local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+//define a connection string to be able to connect to the database.
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/registration_numbersDB';
+
+const pool = new Pool({
+    connectionString
+    // ssl: useSSL
+});
+
+// define instance of factory function
+let reg_number = Reg_numbers(pool);
 //configure express handlebars
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -35,8 +49,10 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-
+// define a POST ROUTE HANDLER TO ENTER REGISTATIONS INTO THE DATABASE
+app.post('', (req, res) => {
+    
+});
 let PORT = process.env.PORT || 3020;
 app.listen(PORT, () => {
     console.log('app starting on PORT', PORT);
--- todo_list.js
+++ todo_list.js
@@ -1,4 +1,5 @@
 // 1) define a function call setRegistrations that that take in one parameter call numbers
-        // - check if registration number fall within our scope.
+        // - define a variable to check if registration number fall within our scope.
+        // - check if registration corresponds with regex.
         // - then if that condition is true, if true add that registration
         // - else false display a message registreation already exists

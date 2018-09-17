0059b3d2f1414513b04084968d5303cc32d80f40
Theophelus <aneletom10@gmail.com>
2018-09-17 10:46:56 +0200

    updating table schemas

--- tables.sql
+++ tables.sql
@@ -1,3 +1,5 @@
+DROP table if exists towns, registration_numbers;
+
 create table towns
 (
     id serial primary key,
@@ -10,5 +12,11 @@ create table registration_numbers
     id serial not null primary key,
     registration_plates text not null,
     towns_id int,
-    foreign key (towns_id) references towns(id)
+    foreign key (towns_id) references towns(id) on DELETE CASCADE
 );
+
+insert into towns(town_names, town_code) values('Cape Town', 'CA');
+insert into towns(town_names, town_code) values('Paarl', 'CL');
+insert into towns(town_names, town_code) values('George', 'CAW');
+insert into towns(town_names, town_code) values('Stellenbosch', 'CJ');
+
